699940262f8bcf3d0291bd309668e6a185cb47ea
kadyrbekov <kadyrbekov@users.noreply.github.com>
2021-02-03 19:56:24 +0600

    added books.html

--- app.py
+++ app.py
@@ -16,38 +16,44 @@ def books():
     excel = load_workbook("tales.xlsx")
     page = excel["Sheet1"]
 
-    #tales = [tale.value for tale in page["A"]][1:]
-    tales = []
 
-    for tale in page["A"][1:]:
-        tales.append(tale.value)
+    object_list= [[tale.value, tale.offset(column=1).value] for tale in page["A"][1:]]
+    print(object_list)
+    return render_template('books.html', object_list=object_list)
 
 
-    authors = [author.value for author in page["B"]][1:]
+    # #tales = [tale.value for tale in page["A"]][1:]
+    # tales = []
 
-    html = """
+    # for tale in page["A"][1:]:
+    #     tales.append(tale.value)
+
+
+    # authors = [author.value for author in page["B"]][1:]
+
+    # html = """
     
-        <a href="/authors">Authors</a>
-        <a href="/books">Books</a>
-        <a href="/">Homepage</a>
-        <h1>Тут будет список книг:</h1>
-    
-    """
-
-    for i in range(len(tales)):
-        html += f"<h2>{tales[i]} - {authors[i]}</h2>"
-
-    # return f"""
+    #     <a href="/authors">Authors</a>
+    #     <a href="/books">Books</a>
+    #     <a href="/">Homepage</a>
     #     <h1>Тут будет список книг:</h1>
-    #     <h2>{tales[0]} - {authors[0]}</h2>
-    #     <h2>{tales[1]} - {authors[1]}</h2>
-    #     <h2>{tales[2]} - {authors[2]}</h2>
-    #     <h2>{tales[3]} - {authors[3]}</h2>
-    #     <h2>{tales[4]} - {authors[4]}</h2
-        
+    
     # """
 
-    return html
+    # for i in range(len(tales)):
+    #     html += f"<h2>{tales[i]} - {authors[i]}</h2>"
+
+    # # return f"""
+    # #     <h1>Тут будет список книг:</h1>
+    # #     <h2>{tales[0]} - {authors[0]}</h2>
+    # #     <h2>{tales[1]} - {authors[1]}</h2>
+    # #     <h2>{tales[2]} - {authors[2]}</h2>
+    # #     <h2>{tales[3]} - {authors[3]}</h2>
+    # #     <h2>{tales[4]} - {authors[4]}</h2
+        
+    # # """
+
+    # return html
 
 
 @app.route("/authors")
@@ -69,7 +75,6 @@ def add():
     page[f"A{last}"] = f["book"]
     page[f"B{last}"] = f["author"]
     excel.save("tales.xlsx")
-
 
 
 

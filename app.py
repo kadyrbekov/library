27ddaf99aa713c4a4ed837ccda6f64699838c1a9
kadyrbekov <kadyrbekov@users.noreply.github.com>
2021-02-22 19:31:34 +0600

    added new

--- app.py
+++ app.py
@@ -18,42 +18,8 @@ def books():
 
 
     object_list= [[tale.value, tale.offset(column=1).value] for tale in page["A"][1:]]
-    print(object_list)
+    print("Books page!")
     return render_template('books.html', object_list=object_list)
-
-
-    # #tales = [tale.value for tale in page["A"]][1:]
-    # tales = []
-
-    # for tale in page["A"][1:]:
-    #     tales.append(tale.value)
-
-
-    # authors = [author.value for author in page["B"]][1:]
-
-    # html = """
-    
-    #     <a href="/authors">Authors</a>
-    #     <a href="/books">Books</a>
-    #     <a href="/">Homepage</a>
-    #     <h1>Тут будет список книг:</h1>
-    
-    # """
-
-    # for i in range(len(tales)):
-    #     html += f"<h2>{tales[i]} - {authors[i]}</h2>"
-
-    # # return f"""
-    # #     <h1>Тут будет список книг:</h1>
-    # #     <h2>{tales[0]} - {authors[0]}</h2>
-    # #     <h2>{tales[1]} - {authors[1]}</h2>
-    # #     <h2>{tales[2]} - {authors[2]}</h2>
-    # #     <h2>{tales[3]} - {authors[3]}</h2>
-    # #     <h2>{tales[4]} - {authors[4]}</h2
-        
-    # # """
-
-    # return html
 
 
 @app.route("/authors")
@@ -61,7 +27,13 @@ def authors():
     excel = load_workbook("tales.xlsx")
     page = excel["Sheet1"]
     authors = {author.value for author in page["B"][1:]}
-    return render_template("authors.html")
+    authors_all = [author.value for author in page["B"][1:]]
+    counts = []
+    for author in authors_all:
+        i = authors_all.count(author)
+        counts.append(i)
+    
+    return render_template("authors.html", authors=authors, authors_all=authors_all, counts=counts)
 
 
 @app.route("/add/", methods=["POST"])
@@ -80,3 +52,38 @@ def add():
 
     return "Success!"
 
+
+@app.route("/book/<num>/")
+def book(num):
+   
+    excel = load_workbook("tales.xlsx")
+    page = excel["Sheet1"]
+
+    object_list = [[tale.value, tale.offset(column=1).value, tale.offset(column=2).value] for tale in page["A"][1:]]
+    obj = object_list[int(num)] # object_list[5]
+    obj.append(num)
+    return render_template("book.html", obj=obj) # **kwargs
+
+
+@app.route("/book/<num>/edit/")
+def book_edit(num):
+    num = int(num) + 2
+    excel_file = load_workbook("tales.xlsx")
+    page = excel_file["Sheet1"]
+    tale = page[f"A{num}"]
+    author = page[f"B{num}"]
+    image = page[f"C{num}"]
+    obj = [tale.value, author.value, image.value, num]
+    return render_template("book_edit.html", obj=obj)
+
+@app.route("/book/<num>/save/", methods=["POST"])
+def book_save(num):
+    num = int(num)
+    excel_file = load_workbook("tales.xlsx")
+    page = excel_file["Sheet1"]
+    form = request.form
+    page[f"A{num}"] = form["tale"]
+    page[f"B{num}"] = form["author"]
+    page[f"C{num}"] = form["image"]
+    excel_file.save("tales.xlsx")
+    return "Сохранено!"
